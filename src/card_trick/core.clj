0a8f045c3a10e69b99c053427ace518bb0564c78
Vinay Ramasesh <ramasesh@google.com>
2021-12-18 14:53:31 -0800

    first commit

---
+++ src/card_trick/core.clj
@@ -0,0 +1,7 @@
+(ns card-trick.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/card_trick/core_test.clj
@@ -0,0 +1,7 @@
+(ns card-trick.core-test
+  (:require [clojure.test :refer :all]
+            [card-trick.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
